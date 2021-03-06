1c40cf4a7ed4d59bbebddaccb89c1248690a76f6
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-03-07 00:50:59 +0600

    module end

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import fakeData from "../../../fakeData";
+import { addToDatabaseCart } from "../../../utilities/databaseManager";
 import Cart from "../../Cart/Cart";
 import Product from "../../Product/Product";
 import "./Shop.css";
@@ -13,6 +14,9 @@ const Shop = () => {
     // console.log('Product added', product);
     const newCart = [...cart, product];
     setCart(newCart);
+    const sameProduct = newCart.filter((pd) => pd.key === product.key);
+    const count = sameProduct.length;
+    addToDatabaseCart(product.key, count);
   };
   //console.log(fakeData);
   return (
@@ -20,6 +24,7 @@ const Shop = () => {
       <div className="product-container">
         {products.map((pd) => (
           <Product
+            key={pd.key}
             showAddToCart={true}
             handleAddProduct={handleAddProduct}
             product={pd}
